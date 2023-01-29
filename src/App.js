5e02ede611e1a9d67feaef77b009012a1da91b4a
Jose J. Lopez-Perez <97471636+JJWesker10@users.noreply.github.com>
2023-01-29 18:41:02 -0500

    added new project text (#3)
    Co-authored-by: samilieberman <samilieberman1@gmail.com>
    Co-authored-by: Jose J. Lopez-Perez <jjwesker10@JJWesker-Nagame>

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+ReleaseBranch
+HEAD
+        <img src={gatorLogo} className="App-logo" alt="logo" />
+        <p>
+          Hello World! This is our team project.
+        </p>	
+
+        <img src={gatorLogo} className="App-logo" alt="logo" />
+        <p> Hello World! </p>
+        <p> My name is Jose J. Lopez-Perez and this is my project </p>
+
  newText
         <img src={gatorLogo} className="App-logo" alt="logo" />
         <p>
